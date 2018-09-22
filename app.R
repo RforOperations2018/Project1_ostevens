137bdb83823083703b6bbc0f4b1d9c8fdf03c8a6
Owen <ostevens@andrew.cmu.edu>
2018-09-22 15:17:06 -0400

    tried to get better GDP per capita measure and abandoned

--- app.R
+++ app.R
@@ -28,8 +28,16 @@ happiness <- origdata %>%
          Gini_Income = gini_of_household_income_reported_in_gallup_by_wp5_year, 
          Gini_Average = gini_index_world_bank_estimate_average_2000_13,
          continent = countrycode(sourcevar = country, origin = "country.name",destination = "continent"),
-         country = as.factor(ifelse(country == "Kosovo", "Europe", as.character(country)))) %>%
-  select(country:perceptions_of_corruption, Gini_Income, Gini_Average, confidence_in_gov, continent)
+         region = countrycode(sourcevar = country, origin = "country.name", destination = "region"),
+         continent = as.factor(ifelse(country == "Kosovo", "Europe", as.character(continent)))) %>%
+  select(country:perceptions_of_corruption, Gini_Income, Gini_Average, confidence_in_gov, continent, region)
+
+
+
+
+# ggplot(twenty16, aes(x = reorder(continent, -life_ladder), life_ladder, fill = continent)) + stat_summary(fun.y = "mean", geom = "bar")
+
+# ggplot(twenty16, aes(x = reorder(country, -life_ladder), y = life_ladder, fill = continent)) + geom_bar(stat = "identity")
 
 
 
